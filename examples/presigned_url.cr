38ba087cacb86c5094435cf18b0b07540fd1f91e
Taylor Finnell <tmfinnell@gmail.com>
2017-07-12 07:46:47 -0400

    update examples

--- examples/presigned_url.cr
+++ examples/presigned_url.cr
@@ -7,16 +7,20 @@ KEY    = ENV["AWS_KEY"]
 SECRET = ENV["AWS_SECRET"]
 REGION = ENV["AWS_REGION"]
 
-creds = Awscr::Signer::Credentials.new(KEY, SECRET)
-
 object = "/#{SecureRandom.uuid}"
 
-options = Awscr::S3::Presigned::Url::Options.new(object: object, bucket: BUCKET, additional_options: {
+options = Awscr::S3::Presigned::Url::Options.new(
+  region: REGION,
+  object: object, 
+  bucket: BUCKET, 
+  aws_access_key: KEY,
+  aws_secret_key: SECRET,
+  additional_options: {
   "x-amz-acl"    => "public-read",
   "Content-Type" => "image/png",
 })
 
-url = Awscr::S3::Presigned::Url.new(REGION, creds, options)
+url = Awscr::S3::Presigned::Url.new(options)
 
 HTTP::Client.put(url.for(:put), HTTP::Headers.new, body: "Howdy!")
 resp = HTTP::Client.get(url.for(:get))
