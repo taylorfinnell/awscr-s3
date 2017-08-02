04479f191eace388d80924a710544cb396e58cac
Taylor Finnell <tmfinnell@gmail.com>
2017-08-02 16:49:35 -0400

    http tests

--- src/awscr-s3/http.cr
+++ src/awscr-s3/http.cr
@@ -26,39 +26,31 @@ module Awscr::S3
     def delete(path)
       resp = @http.delete(path)
       handle_response!(resp)
-      resp
     end
 
     def post(path, body = nil)
       resp = @http.post(path, body: body)
       handle_response!(resp)
-      resp
     end
 
     def put(path : String, body : IO | String)
       resp = @http.put(path, headers: HTTP::Headers{"Content-Length" => body.size.to_s}, body: body)
       handle_response!(resp)
-      resp
     end
 
     def head(path)
       resp = @http.head(path)
       handle_response!(resp)
-      resp
     end
 
     def get(path)
       resp = @http.get(path)
       handle_response!(resp)
-      resp
     end
 
     private def handle_response!(response)
-      # return true if the response is fine
-      return true if (200..299).includes?(response.status_code)
+      return response if (200..299).includes?(response.status_code)
 
-      # ok the server said what is wrong, if there is a body in the response we
-      # can get a specific message, otherwise we reraise a server error
       if !response.body.empty?
         raise ServerError.from_response(response)
       else
