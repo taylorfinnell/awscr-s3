55e95bc276cc9cc079d11e4974b81cb3119759de
Taylor Finnell <tmfinnell@gmail.com>
2017-08-02 16:49:03 -0400

    examples

---
+++ examples/list_objects.cr
@@ -0,0 +1,16 @@
+require "../src/awscr-s3"
+
+BUCKET = ENV["AWS_BUCKET"]
+KEY    = ENV["AWS_KEY"]
+SECRET = ENV["AWS_SECRET"]
+REGION = ENV["AWS_REGION"]
+
+client = Awscr::S3::Client.new(
+  region: REGION,
+  aws_access_key: KEY,
+  aws_secret_key: SECRET
+)
+
+client.list_objects(bucket: BUCKET, max_keys: 10).each do |response|
+  p response.contents.map(&.key)
+end
