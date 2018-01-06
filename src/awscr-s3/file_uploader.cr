4023e09fc730d9e3b71acf155d35a9c24f3cd36e
Taylor Finnell <tmfinnell@gmail.com>
2018-01-05 19:06:50 -0500

    remove dead code

--- src/awscr-s3/file_uploader.cr
+++ src/awscr-s3/file_uploader.cr
@@ -2,9 +2,8 @@ module Awscr::S3
   # Uploads a file to S3. If the file is 5MB it is uploaded in a single request.
   # If the file is greater than 5MB it is uploaded in parts.
   class FileUploader
+    # :nodoc:
     UPLOAD_THRESHOLD = 5_000_000 # 5mb
-
-    getter client
 
     def initialize(@client : Client)
     end
@@ -17,9 +16,9 @@ module Awscr::S3
     # ```
     def upload(bucket : String, object : String, io : IO, headers : Hash(String, String) = Hash(String, String).new)
       if io.size < UPLOAD_THRESHOLD
-        client.put_object(bucket, object, io, headers)
+        @client.put_object(bucket, object, io, headers)
       else
-        uploader = MultipartFileUploader.new(client)
+        uploader = MultipartFileUploader.new(@client)
         uploader.upload(bucket, object, io, headers)
       end
     end
