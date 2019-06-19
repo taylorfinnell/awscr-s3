bec29cd30d6e5b556e187962ba1c2e31494bdf0c
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2019-06-19 16:47:46 +0100

    Add head_object method

---
+++ src/awscr-s3/head_object_output.cr
@@ -0,0 +1,29 @@
+require "xml"
+
+module Awscr::S3::Response
+  class HeadObjectOutput
+    # The body of the request object
+    getter status : HTTP::Status
+    getter status_message : String | Nil
+    getter headers : HTTP::Headers
+    getter content_type : String
+    getter last_modified : String
+    getter size : UInt64
+
+    # Create a `GetObjectOutput` response from an
+    # `HTTP::Client::Response` object
+    def self.from_response(response)
+      new(
+        status: response.status,
+        status_message: response.status_message,
+        headers: response.headers,
+        content_type: response.headers["Content-Type"],
+        last_modified: response.headers["Last-Modified"],
+        size: response.headers["Content-Length"].to_u64,
+      )
+    end
+
+    def initialize(@status, @status_message, @headers, @content_type, @last_modified, @size)
+    end
+  end
+end
