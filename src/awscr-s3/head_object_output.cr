716e56f5bbe87e24461bbec560fb79f3f138981d
Mitch <2813287+acoustep@users.noreply.github.com>
2019-06-19 17:28:04 +0100

    Parse last_modified date into Time instance

--- src/awscr-s3/head_object_output.cr
+++ src/awscr-s3/head_object_output.cr
@@ -1,11 +1,14 @@
 module Awscr::S3::Response
   class HeadObjectOutput
+    
+    DATE_FORMAT = "%FT%T"
+    
     # The body of the request object
     getter status : HTTP::Status
     getter status_message : String | Nil
     getter headers : HTTP::Headers
     getter content_type : String
-    getter last_modified : String
+    getter last_modified : Time
     getter size : UInt64
 
     # Create a `GetObjectOutput` response from an
@@ -16,7 +19,7 @@ module Awscr::S3::Response
         status_message: response.status_message,
         headers: response.headers,
         content_type: response.headers["Content-Type"],
-        last_modified: response.headers["Last-Modified"],
+        last_modified: Time.parse(response.headers["Last-Modified"], DATE_FORMAT, Time::Location::UTC),
         size: response.headers["Content-Length"].to_u64,
       )
     end
