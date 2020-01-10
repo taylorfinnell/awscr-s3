827d6dcfc1ebc710a590227b2dd4a5f152075639
Taylor Finnell <tmfinnell@gmail.com>
2020-01-10 18:26:33 -0500

    Add more exceptions

--- src/awscr-s3/exceptions.cr
+++ src/awscr-s3/exceptions.cr
@@ -1,18 +1,89 @@
 module Awscr::S3
-  private EXCEPTIONS = [
-    "BucketAlreadyExists",
-    "BucketAlreadyOwnedByYou",
-    "NoSuchBucket",
-    "NoSuchKey",
-    "NoSuchUpload",
-    "ObjectAlreadyInActiveTierError",
-    "ObjectNotInActiveTierError",
-  ]
+  private EXCEPTIONS = %w(
+    AccountProblem
+    AllAccessDisabled
+    AmbiguousGrantByEmailAddress
+    BadDigest
+    BucketAlreadyExists
+    BucketAlreadyOwnedByYou
+    BucketNotEmpty
+    CredentialsNotSupported
+    CrossLocationLoggingProhibited
+    EntityTooSmall
+    EntityTooLarge
+    ExpiredToken
+    IllegalVersioningConfigurationException
+    IncompleteBody
+    IncorrectNumberOfFilesInPostRequest
+    InlineDataTooLarge
+    InternalError
+    InvalidAccessKeyId
+    InvalidAddressingHeader
+    InvalidArgument
+    InvalidBucketName
+    InvalidBucketState
+    InvalidDigest
+    InvalidEncryptionAlgorithmError
+    InvalidLocationConstraint
+    InvalidObjectState
+    InvalidPart
+    InvalidPartOrder
+    InvalidPayer
+    InvalidPolicyDocument
+    InvalidRange
+    InvalidRequest
+    InvalidSecurity
+    InvalidSOAPRequest
+    InvalidStorageClass
+    InvalidTargetBucketForLogging
+    InvalidToken
+    InvalidURI
+    KeyTooLongError
+    MalformedACLError
+    MalformedPOSTRequest
+    MalformedXML
+    MaxMessageLengthExceeded
+    MaxPostPreDataLengthExceededError
+    MetadataTooLarge
+    MethodNotAllowed
+    MissingAttachment
+    MissingContentLength
+    MissingRequestBodyError
+    MissingSecurityElement
+    MissingSecurityHeader
+    NoLoggingStatusForKey
+    NoSuchBucket
+    NoSuchBucketPolicy
+    NoSuchKey
+    NoSuchLifecycleConfiguration
+    NoSuchUpload
+    NoSuchVersion
+    NotImplemented
+    NotSignedUp
+    OperationAborted
+    PermanentRedirect
+    PreconditionFailed
+    Redirect
+    RestoreAlreadyInProgress
+    RequestIsNotMultiPartContent
+    RequestTimeout
+    RequestTimeTooSkewed
+    RequestTorrentOfBucketError
+    ServerSideEncryptionConfigurationNotFoundError
+    ServiceUnavailable
+    SignatureDoesNotMatch
+    SlowDown
+    TemporaryRedirect
+    TokenRefreshRequired
+    TooManyBuckets
+    UnexpectedContent
+    UnresolvableGrantByEmailAddress
+    UserKeyMustBeSpecified)
 
   # Exception raised when S3 gives us a non 200 http status code. The error
   # will have a specific message from S3.
   class Exception < ::Exception
-    # Creates a `ServerError` from an `HTTP::Client::Response`
+    # Creates a `S3::Exception` from an `HTTP::Client::Response`
     def self.from_response(response)
       {% begin %}
         xml = XML.new(response.body || response.body_io)
