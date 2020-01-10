005e2eb96bd7526cb5b5884858b7b66c496596d8
Carl Hörberg <carl.hoerberg@gmail.com>
2020-01-10 16:10:41 +0100

    Make GetObjectObject#body always be IO
    Currently code using this has to check which type it is.

--- src/awscr-s3/responses/get_object_output.cr
+++ src/awscr-s3/responses/get_object_output.cr
@@ -10,10 +10,10 @@ module Awscr::S3::Response
     # Create a `GetObjectOutput` response from an
     # `HTTP::Client::Response` object
     def self.from_response(response)
-      new(response.body? || response.body_io, response.headers)
+      new(response.body_io, response.headers)
     end
 
-    def initialize(@body : IO | String, @headers : HTTP::Headers)
+    def initialize(@body : IO, @headers : HTTP::Headers)
     end
 
     def_equals @body
