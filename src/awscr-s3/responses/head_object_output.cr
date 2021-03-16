17c5a4420f5d2acc2e733bd5efa693b16436e9bf
Mikhail Arzhaev <marzhaev@texenergo.com>
2021-03-16 20:17:01 +0300

    Move head_object_output to the dedicated folder. Add frequently used etag and meta accessors.

--- src/awscr-s3/responses/head_object_output.cr
+++ src/awscr-s3/responses/head_object_output.cr
@@ -9,10 +9,18 @@ module Awscr::S3::Response
     getter content_type : String
     getter last_modified : Time
     getter size : UInt64
+    getter etag : String
+    getter meta : Hash(String, String)
 
     # Create a `GetObjectOutput` response from an
     # `HTTP::Client::Response` object
-    def self.from_response(response)
+    def self.from_response(response : HTTP::Client::Response)
+      meta = {} of String => String
+      response.headers.each do |k, v|
+        next unless k.starts_with?("x-amz-meta-")
+        meta[k.lchop("x-amz-meta-")] = v.first
+      end
+
       new(
         status: response.status,
         status_message: response.status_message,
@@ -20,10 +28,12 @@ module Awscr::S3::Response
         content_type: response.headers["Content-Type"],
         last_modified: self.parse_date(response.headers["Last-Modified"]),
         size: response.headers["Content-Length"].to_u64,
+        etag: response.headers["ETag"].strip('"'),
+        meta: meta
       )
     end
 
-    def initialize(@status, @status_message, @headers, @content_type, @last_modified, @size)
+    def initialize(@status, @status_message, @headers, @content_type, @last_modified, @size, @etag, @meta)
     end
 
     private def self.parse_date(date : String)
