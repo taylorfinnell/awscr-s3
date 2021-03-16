8c5aabc15eff05f1861d6fe450e09b4b1a043ec6
Mikhail Arzhaev <marzhaev@texenergo.com>
2021-03-16 20:33:41 +0300

    Move frequently accessed response data from class init to class's instance methods.

--- src/awscr-s3/responses/head_object_output.cr
+++ src/awscr-s3/responses/head_object_output.cr
@@ -6,37 +6,48 @@ module Awscr::S3::Response
     getter status : HTTP::Status
     getter status_message : String | Nil
     getter headers : HTTP::Headers
-    getter content_type : String
-    getter last_modified : Time
-    getter size : UInt64
-    getter etag : String
-    getter meta : Hash(String, String)
 
     # Create a `GetObjectOutput` response from an
     # `HTTP::Client::Response` object
     def self.from_response(response : HTTP::Client::Response)
+      new(
+        status: response.status,
+        status_message: response.status_message,
+        headers: response.headers
+      )
+    end
+
+    def initialize(@status, @status_message, @headers)
+    end
+
+    {% for f in ["Cache-Control", "Content-Disposition", "Content-Encoding", "Content-Language", "Content-Type",] %}
+      def {{ f.id.stringify.underscore.gsub(/-/, "_").id }} : String?
+        headers["{{ f.id }}"]?
+      end
+    {% end %}
+
+    def last_modified : Time
+      parse_date(headers["Last-Modified"])
+    end
+
+    def size : UInt64
+      headers["Content-Length"].to_u64
+    end
+
+    def etag : String?
+      headers["ETag"].try{|v| v.strip('"')}
+    end
+
+    def meta : Hash(String, String)
       meta = {} of String => String
       response.headers.each do |k, v|
         next unless k.starts_with?("x-amz-meta-")
         meta[k.lchop("x-amz-meta-")] = v.first
       end
-
-      new(
-        status: response.status,
-        status_message: response.status_message,
-        headers: response.headers,
-        content_type: response.headers["Content-Type"],
-        last_modified: self.parse_date(response.headers["Last-Modified"]),
-        size: response.headers["Content-Length"].to_u64,
-        etag: response.headers["ETag"].strip('"'),
-        meta: meta
-      )
+      meta
     end
 
-    def initialize(@status, @status_message, @headers, @content_type, @last_modified, @size, @etag, @meta)
-    end
-
-    private def self.parse_date(date : String)
+    private def parse_date(date : String)
       Time.parse!(date.gsub(/\s{2,}/, ' '), DATE_FORMAT)
     end
   end
