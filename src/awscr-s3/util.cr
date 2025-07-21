3df35b5215810abd6dd09a668fba0671079f4815
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2025-07-21 11:33:02 +0200

    Temporarily skip warnings for util_spec.cr

--- src/awscr-s3/util.cr
+++ src/awscr-s3/util.cr
@@ -1,5 +1,6 @@
 module Awscr::S3
   module Util
+    # Remove --exclude-warnings=spec/awscr-s3/util_spec.cr from scripts/run_batched_specs on removal
     @[Deprecated("Use URI.encode_path instead")]
     def self.encode(object_id : String) : String
       String.build do |io|
